41d27e00cece9659c06262e2766fa011bf146d5b
Jose <email>
2020-07-18 09:58:08 -0600

    create C Busqueda M Catalogo

--- src/app/catalogo/catalogo.module.ts
+++ src/app/catalogo/catalogo.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CatalogoRoutingModule } from './catalogo-routing.module';
 import { CatalogoComponent } from './catalogo.component';
 import { ListaComponent } from './lista/lista.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
 
 
 @NgModule({
-  declarations: [CatalogoComponent, ListaComponent],
+  declarations: [CatalogoComponent, ListaComponent, BusquedaComponent],
   imports: [
     CommonModule,
     CatalogoRoutingModule
