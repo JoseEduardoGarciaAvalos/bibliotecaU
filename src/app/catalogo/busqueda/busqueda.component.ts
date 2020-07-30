668b24f2221c36c06484fb1e736a380a3cbafb40
Jose <email>
2020-07-30 12:00:54 -0600

    statefull Campo Busqueda Libros, eventos rxjs

--- src/app/catalogo/busqueda/busqueda.component.ts
+++ src/app/catalogo/busqueda/busqueda.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CatalogoService } from "../catalogo.service";
 import { UtilService } from "../../util.service";
-import { Router } from "@angular/router";
+import { Router, ActivationStart} from "@angular/router";
+import { filter, map } from "rxjs/operators";
 
 @Component({
   selector: 'app-busqueda',
@@ -10,19 +11,30 @@ import { Router } from "@angular/router";
 })
 export class BusquedaComponent implements OnInit {
 
+  private subruta: string = "general";
+  public valorCampoInicial: string = "";
+
   constructor(
     private CatalogoService:CatalogoService,
     private util:UtilService,
-    private route:Router
+    private router:Router,
   ) { }
 
   ngOnInit() {
+    this.router.events.pipe(
+      filter( evt => evt instanceof ActivationStart ),
+      map( (evt:ActivationStart) => [evt.snapshot.data, evt.snapshot.params])
+    ).subscribe( (data) => {
+      this.subruta = data[0]["sublista"] ? data[0]["sublista"]: "general";
+      this.valorCampoInicial = data[1]["buscar"] ? data[1]["buscar"]: "";
+      this.util.consola("BusquedaComponent"," (ngOnInit) " + this.subruta + "/" + this.valorCampoInicial);
+    })
 
   }
 
   buscar(palabras: string){
-    this.util.consola("BusquedaComponent"," (buscar) " + palabras);
-    this.route.navigate(['/catalogo','general',palabras]);
+    this.util.consola("BusquedaComponent",` (buscar) ${this.subruta}/${palabras}`);
+    this.router.navigate(['/catalogo',this.subruta,palabras]);
   }
 
 }
