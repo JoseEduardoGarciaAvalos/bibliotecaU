4cfde8d9c64316326ec084b5a17c95e45875a29f
Jose <email>
2020-07-31 12:37:31 -0600

    routing Home y 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Page404Component } from './page404/page404.component';
 
 
-const routes: Routes = [{ path: 'catalogo', loadChildren: () => import('./catalogo/catalogo.module').then(m => m.CatalogoModule) }, { path: 'estante', loadChildren: () => import('./estante/estante.module').then(m => m.EstanteModule) }];
+
+const routes: Routes = [
+  { path: 'catalogo', loadChildren: () => import('./catalogo/catalogo.module').then(m => m.CatalogoModule) },
+  { path: 'estante', loadChildren: () => import('./estante/estante.module').then(m => m.EstanteModule) },
+  { path: 'home', component: HomeComponent},
+  { path: '', pathMatch:'full', redirectTo: "/home"},
+  { path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
