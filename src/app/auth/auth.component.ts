600c0aff2d7c2ab175b8dd8661c63954f7db77d8
Jose <email>
2020-07-31 12:53:33 -0600

    localStorage Usuario y notificaciones

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../auth.service";
+import { UtilService } from "src/app/util.service";
+import { Router} from "@angular/router";
 
 @Component({
   selector: 'app-auth',
@@ -8,7 +10,17 @@ import { AuthService } from "../auth.service";
 })
 export class AuthComponent implements OnInit {
 
-  constructor(public auth: AuthService) { }
+  constructor(
+    public auth: AuthService,
+    private util: UtilService,
+    private router: Router
+  ) { 
+    const cliente = localStorage.getItem("cliente");
+    if(cliente){
+      this.auth.cliente = JSON.parse(cliente);
+      this.auth.isLogin = true;
+    }
+  }
 
   ngOnInit() {
   }
@@ -19,6 +31,11 @@ export class AuthComponent implements OnInit {
         if(res[0]){
           this.auth.cliente = res[0];
           this.auth.isLogin = true;
+          localStorage.setItem("cliente", JSON.stringify(this.auth.cliente));
+          this.router.navigate(["/home"]);
+          this.util.notificacion("Inició sesión correctamente", 3);
+        } else {
+          this.util.notificacion("Usuario/contraseña son inválidos", 3);
         }
       },
       //(error) => {this.auth.testContectar();}
@@ -27,6 +44,9 @@ export class AuthComponent implements OnInit {
 
   salir(){
     this.auth.desconectar();
+    this.router.navigate(["/home"]);
+    localStorage.removeItem("cliente");
+    this.util.notificacion("Cerró sesión correctamente", 3);
   }
 
 }
